b0fbc731a0cd450ced802ea88bf983b7d0f132ac
Chennex <s164147@student.dtu.dk>
2019-01-15 15:13:32 +0100

    Add files via upload

--- RFID.ino
+++ RFID.ino
@@ -1,12 +1,41 @@
 //RFID based on serial.
 
 #include <SoftwareSerial.h>
+#include <SpritzCipher.h>
 
 SoftwareSerial SoftSerial(10,11); //rx,tx
 
-char buffer[63];
+byte buffer[63];
 int count = 0;
 char tag[10];
+
+String hashRFID(byte data[], byte dataLength){
+  byte hashLength = 1;
+  byte digest[dataLength]; //out buffer.
+  byte digest_2[hashLength];
+
+  spritz_ctx hash_ctx;
+
+  unsigned int i;
+
+  spritz_hash_setup(&hash_ctx);
+  
+  for(i = 0; i < dataLength; i++){
+    spritz_hash_update(&hash_ctx, data + i, 1);
+  }
+
+  spritz_hash_final(&hash_ctx, digest_2, hashLength);
+
+  spritz_hash(digest, hashLength, data, dataLength);
+
+  spritz_hash(digest, hashLength, data, dataLength);
+
+  String output;
+  for(i = 0; i < sizeof(digest); i++){
+    output += String(digest[i]);
+  }
+  return output;
+}
 
 void setup() {
   SoftSerial.begin(9600);
@@ -20,14 +49,17 @@ void loop() {
     buffer[count] = SoftSerial.read();
     count++;
   }
+  String hashed = hashRFID(buffer, count);
   
-  Serial.write(buffer,count);
+  Serial.println(hashed);
   count = 0;
-
+  delay(5000);
   //Debugging code to write to serial RFID transmitter.
   if(Serial.available()){
     SoftSerial.write(Serial.read());
   }
 }
+
+
 
 
